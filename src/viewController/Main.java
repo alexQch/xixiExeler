f791d0d093b7603c2d155cdbe5a4b89a73fd02e2
chenhui <alex.qch@gmail.com>
2016-07-28 17:42:39 +0800

    removed unneeded comment

--- src/viewController/Main.java
+++ src/viewController/Main.java
@@ -2,7 +2,6 @@ package viewController;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
